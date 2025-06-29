e287c015bf1da37d5e730b73fe1bc3935f75f2e7
xjiaf <japhenxiong@outlook.com>
2025-06-29 10:16:20 +0800

    update data

--- data/multi30k/create_test_val_flickr.py
+++ data/multi30k/create_test_val_flickr.py
@@ -1,26 +1,66 @@
-import os
-import sys
+import shutil
+from pathlib import Path
 
-def main(list_file_path, source_folder, destination_folder):
-    with open(list_file_path, 'r') as file_list:
-        for line in file_list:
-            # Strip newline character and any leading/trailing spaces from the line
-            file_name = line.strip()
 
-            # Check if the file exists before moving
-            if os.path.isfile(file_name):
-                # Use os.path.basename to get the filename without the path
-                file_name_only = os.path.basename(file_name)
+def create_directories(base_path):
+    image_dir = base_path / 'images'
+    # text_dir = base_path / 'text'
+    # text_train_dir = text_dir / 'text_train'
+    # text_val_dir = text_dir / 'text_val'
+    # text_test_dir = text_dir / 'text_test'
+    image_train_dir = image_dir / 'train'
+    image_val_dir = image_dir / 'test_2016_val'
+    image_test_dir = image_dir / 'test_2016_flickr'
 
-                # Move the file to the destination folder
-                source_path = os.path.join(source_folder, file_name_only)
-                destination_path = os.path.join(destination_folder, file_name_only)
-                os.rename(source_path, destination_path)
-            else:
-                print(f"File not found: {file_name}")
+    # dirs = [processed_dir, multi30k_dir, text_train_dir, text_val_dir, text_test_dir, image_train_dir, image_val_dir, image_test_dir]
+    dirs = [image_dir, image_train_dir, image_val_dir, image_test_dir]
+    for dir in dirs:
+        dir.mkdir(parents=True, exist_ok=True)
+    # return text_train_dir, text_val_dir, text_test_dir, image_train_dir, image_val_dir, image_test_dir
+    return image_train_dir, image_val_dir, image_test_dir
 
-if __name__ == '__main__':
-    file_list_path = sys.argv[1]
-    source_folder = sys.argv[2]
-    destination_folder = sys.argv[3]
-    main(file_list_path, source_folder, destination_folder)
+
+def move_text_files(task1_tok_dir, text_train_dir, text_val_dir,
+                    text_test_dir):
+    for file in task1_tok_dir.iterdir():
+        if file.name.startswith('train'):
+            shutil.copy2(str(file), str(text_train_dir / file.name))
+        elif file.name.startswith('val'):
+            shutil.copy2(str(file), str(text_val_dir / file.name))
+        elif file.name.startswith('test_2016'):
+            shutil.copy2(str(file), str(text_test_dir / file.name))
+
+
+def move_image_files(image_splits_dir, flickr_images_dir, image_train_dir,
+                     image_val_dir, image_test_dir):
+    splits = {
+        'train.txt': image_train_dir,
+        'val.txt': image_val_dir,
+        'test_2016_flickr.txt': image_test_dir
+    }
+
+    for split_file, target_dir in splits.items():
+        with open(image_splits_dir / split_file, 'r') as f:
+            for line in f:
+                image_file = flickr_images_dir / line.strip()
+                if image_file.exists():
+                    shutil.copy2(str(image_file),
+                                 str(target_dir / image_file.name))
+
+
+def main():
+    base_path = Path('.')
+    data_dir = base_path
+    # task1_tok_dir = data_dir / 'multi30k-dataset' / 'data' / 'task1' / 'tok'
+    image_splits_dir = data_dir / 'multi30k-dataset' / 'data' / 'task1' / 'image_splits'
+    flickr_images_dir = data_dir / 'flickr-image-dataset' / 'flickr30k_images' / 'flickr30k_images'
+
+    image_train_dir, image_val_dir, image_test_dir = create_directories(
+        base_path)
+    # move_text_files(task1_tok_dir, text_train_dir, text_val_dir, text_test_dir)
+    move_image_files(image_splits_dir, flickr_images_dir, image_train_dir,
+                     image_val_dir, image_test_dir)
+
+
+if __name__ == "__main__":
+    main()
--- data/multi30k/setup_multi30k.sh
+++ data/multi30k/setup_multi30k.sh
@@ -1,26 +1,40 @@
+# Clone the repository with submodules
 git clone --recursive https://github.com/multi30k/dataset.git multi30k-dataset
-mv multi30k-dataset/* .
-rm -rf multi30k-dataset
-mkdir images text
-mv data text
-mv scripts text
-cd text/data/task1/raw
-gunzip *.gz
-for file in val.*; do mv "$file" "test_2016_val.${file##*.}"; done
-cd ../image_splits
-mv val.txt test_2016_val.txt
-cd ../../../..
-mv flickr30k-images.tar.gz images 
-mv images_mscoco.task1.tar.gz images
-mv test_2017-flickr-images.gz images
-cd images
-tar -xvzf images_mscoco.task1.tar.gz
-mv translated_images test_2017_mscoco
+
+# Run download script
+python download_flickr.py
+
+# Process test_2017-flickr-images.tar.gz
+mkdir -p ./images
+
 tar -xvzf test_2017-flickr-images.tar.gz
 mv task1 test_2017_flickr
-tar -xvzf flickr30k-images.tar.gz
-mv flickr30k-images train
-mkdir test_2016_flickr test_2016_val
-mv ../create_test_val_flickr.py .
-python create_test_val_flickr.py ../text/data/task1/image_splits/test_2016_val.txt train test_2016_val
-python create_test_val_flickr.py ../text/data/task1/image_splits/test_2016_flickr.txt train test_2016_flickr
+mv test_2017_flickr ./images
+rm -rf test_2017-flickr-images.tar.gz
+
+# Process flickr30k-images.tar.gz and split into train, val, and test
+python create_test_val_flickr.py
+rm -rf flickr-image-dataset
+
+# Process the captions
+mv multi30k-dataset/* .
+rm -rf multi30k-dataset
+mkdir text
+mv data text
+mv scripts text
+
+# Navigate to the raw data folder
+cd text/data/task1/raw
+
+# Unzip all gz files
+gunzip *.gz
+
+# Rename files as needed and change their permissions
+for file in val.*; do
+  mv "$file" "test_2016_val.${file##*.}"
+  chmod 644 "test_2016_val.${file##*.}" # Set read and write for owner, read for others
+done
+
+cd ../image_splits
+mv val.txt test_2016_val.txt
+chmod 644 test_2016_val.txt # Set read and write for owner, read for others
